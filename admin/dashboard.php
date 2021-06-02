7a935170e5e735a307fc638f46ca587ed0119a62
Ganapathy Subramanian S <60843507+GanapathySubramanian@users.noreply.github.com>
2021-06-02 12:32:09 +0530

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,9 +4,9 @@
                     <div class="col-md-12 mt-lg-4 mt-4">
                         <div class="d-sm-flex align-items-center justify-content-between mb-4">
                             <h1 class="h3 mb-0 text-gray-800 text-primary font-weight-bold">Dashboard</h1>
-                            <form method="post" align="center" action="partials/admin_db.php">
+<!--                             <form method="post" align="center" action="partials/admin_db.php">
 			                    <input type="submit" name="admin_export" value="Download report" class="btn btn-success mt-3" />
-		                    </form>
+		                    </form> -->
                         </div>
                     </div>
                     <?php include('../c_dashboard.php');?>  
