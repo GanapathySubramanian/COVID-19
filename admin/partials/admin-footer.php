ab35d9ad0d54688a6d4ae569306e348ff431bec4
Ganapathy Subramanian S <60843507+GanapathySubramanian@users.noreply.github.com>
2021-07-13 13:07:39 +0530

    Update admin-footer.php

--- admin/partials/admin-footer.php
+++ admin/partials/admin-footer.php
@@ -1,9 +1,12 @@
-        <script src="../assets/js/nav.js"></script>    
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="../assets/js/nav.js"></script>
+       
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" ></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
         
+        <script src="../assets/js/pagination.js"></script>
         <script>
             admin();
+
         </script>
     </body>
 </html>
