5f4cb6cc6e0ab2c026c39ed06b8edbd417d14a82
Ganapathy Subramanian S <60843507+GanapathySubramanian@users.noreply.github.com>
2021-07-13 13:35:41 +0530

    Update districts.php

--- users/districts.php
+++ users/districts.php
@@ -5,11 +5,11 @@
         $valueToSearch = $_POST['valueToSearch'];
         // search in all table columns
         // using concat mysql function
-        $query = "SELECT * FROM `district_cases` WHERE  CONCAT(`districts`) LIKE '%".$valueToSearch."%' order by districts";
+        $query = "SELECT * FROM `district_cases` WHERE  CONCAT(`districts`,`state_code`) LIKE '%".$valueToSearch."%' order by state_code";
         $search_result = filterTable($query);
     }
     else {
-        $query = "SELECT * FROM `district_cases` order by districts";
+        $query = "SELECT * FROM `district_cases` order by state_code";
         $search_result = filterTable($query);
     }
 // function to connect and execute the query
@@ -27,14 +27,27 @@
         <a class="navbar-brand text-primary font-weight-bold" href="#"><h3>District Wise Status</h3></a>
         <form class="d-flex"  action="" method="POST" autocomplete="off">
             <input class="form-control me-2" type="search" name="valueToSearch" placeholder="Value To Search" aria-label="Search">
-            <button class="btn ml-2 btn-primary" type="submit" name="search">Search</button>
+            <button class="btn btn-primary ml-2" type="submit" name="search">Search</button>
         </form>
     </nav>
+ <label class="text-primary font-weight-bold"> Select No.of.rows to display :</label>
+    <select class="form-control" name="state" id="maxRows">
+		<option value="5000">Show ALL Rows</option>
+		<option value="5">5</option>
+		<option value="10">10</option>
+		<option value="15">15</option>
+		<option value="20">20</option>
+		<option value="50">50</option>
+		<option value="70">70</option>
+		<option value="100">100</option>
+    </select>
     <div class="table-responsive">
-        <table class="content-table table">
+        <table class="content-table table" id="table-id">
             <thead>
                 <tr>
-                    <th>SNO</th>
+             
+                    
+                    <th>STATE_CODE</th>
                     <th>DISTRICTS</th>
                     <th>CONFIRMED</th>
                     <th>MALE</th>
@@ -46,12 +59,13 @@
             </thead>
 
             <?php 
-            $count=0;
+           
             while($row = mysqli_fetch_array($search_result)):
-            $count+=1;
+            
             ?>
                 <tr>
-                    <td><?php echo $count;?></td>
+                    
+                    <td><?php echo $row['state_code']?></td>
                     <td><?php echo $row['districts']?></td>
                     <td><?php echo $row['confirmed']?></td>
                     <td><?php echo $row['male']?></td>
@@ -63,6 +77,14 @@
             <?php endwhile;?>
         </table>
     </div>
-
+  <div class='pagination-container mt-2'>
+                <nav>
+                  <ul class="pagination">
+                     <li class="page-item" style="cursor:pointer;" data-page="prev" ><span class="page-link"> < <span class="sr-only">(current)</span></span></li>
+                   <!--	Here the JS Function Will Add the Rows -->
+                    <li class="page-item" style="cursor:pointer;"  data-page="next" id="prev"><span class="page-link"> > <span class="sr-only">(current)</span></span></li>
+                  </ul>
+                </nav>
+        </div>
 </div>
 <?php include('partials/user-footer.php');?>
